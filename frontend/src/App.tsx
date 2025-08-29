222b51e7b7f3e0c7e0f899889fe97a241f444649
rivenintech <contact@rivenintech.com>
2025-08-29 20:41:37 +0200

    feat: implement book read toggle and add header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
-import { Loader2 } from "lucide-react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { BookOpen, Loader2 } from "lucide-react";
 import bookCover from "./assets/book.svg";
 import BookCard from "./components/book";
 
@@ -11,6 +11,8 @@ type Book = {
 };
 
 function App() {
+  const queryClient = useQueryClient();
+
   const { data, isLoading } = useQuery({
     queryKey: ["books"],
     queryFn: async () => {
@@ -19,20 +21,50 @@ function App() {
     },
   });
 
+  const mutation = useMutation({
+    mutationFn: async (updatedBook: { id: number; read: boolean }) => {
+      const response = await fetch(`http://localhost:3000/api/books/${updatedBook.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ read: updatedBook.read }),
+      });
+      if (!response.ok) {
+        throw new Error(`Failed to update book: ${response.statusText}`);
+      }
+    },
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ["books"] });
+    },
+  });
+
   return (
     <div className="container mx-auto p-4">
-      {isLoading && (
-        <Loader2 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform animate-spin" />
-      )}
-      {data && (
-        <ul className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-          {data.map((book) => (
-            <li key={book.id} className="text-center">
-                <BookCard bookData={{ ...book, bookCover }} onToggleRead={() => {}} />
-            </li>
-          ))}
-        </ul>
-      )}
+      <header>
+        <div className="items-bottom mb-8 flex gap-2">
+          <BookOpen size={30} className="self-baseline-last" />
+          <h1 className="text-2xl font-bold">Book Tracker</h1>
+        </div>
+      </header>
+      <main>
+        {isLoading && (
+          <Loader2 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform animate-spin" />
+        )}
+        {data && (
+          <ul className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+            {data.map((book) => (
+              <li key={book.id} className="text-center">
+                <BookCard
+                  bookData={{ ...book, bookCover }}
+                  onToggleRead={() => mutation.mutate({ id: book.id, read: !book.read })}
+                />
+              </li>
+            ))}
+          </ul>
+        )}
+      </main>
     </div>
   );
 }
